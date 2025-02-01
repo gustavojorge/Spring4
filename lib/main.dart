98b031effab0e0424ee01f0075c42acff052f3f9
gustavojorge <gustavojorge080@gmail.com>
2025-02-01 17:48:29 -0300

    Todo list - DONE

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacity = true;
 
   @override
   Widget build(BuildContext context) {
@@ -18,23 +25,47 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           leading: Container(),
-          title: Text("Tarefas", style: TextStyle(color: Colors.white),),
+          title: Text(
+            "Tarefas",
+            style: TextStyle(color: Colors.white),
+          ),
           backgroundColor: Colors.blue,
         ),
-        body: ListView(
-          children: [
-            Task("Aprender flutter"),
-            Task("Andar de bike"),
-            Task("Meditar"),
-            Task("Aprender flutter"),
-            Task("Andar de bike"),
-            Task("Meditar"),
-            Task("Aprender flutter"),
-            Task("Andar de bike"),
-            Task("Meditar"),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacity ? 1 : 0,
+          duration: Duration(milliseconds: 800),
+          child: ListView(
+            children: [
+              Task(
+                  "Aprender flutter",
+                  "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",
+                  3),
+              Task(
+                  "Andar de bike",
+                  "https://www.oxerbrasil.com.br/wp-content/uploads/2023/02/sedentarismo-ca.jpg.webp",
+                  2),
+              Task(
+                  "Ler",
+                  "https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg",
+                  0),
+              Task(
+                  "Meditar",
+                  "https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg",
+                  5),
+              Task("Jogar",
+                  "https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg", 1),
+            ],
+          ),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}, backgroundColor: Colors.blue, child: Icon(Icons.add, color: Colors.white),),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacity = !opacity;
+            });
+          },
+          backgroundColor: Colors.blue,
+          child: opacity ? Icon(Icons.remove_red_eye, color: Colors.white) : Icon(Icons.remove_red_eye, color: Colors.green),
+        ),
       ),
     );
   }
@@ -42,8 +73,10 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String nome;
+  final String foto;
+  final int dificuldade;
 
-  const Task(this.nome, {super.key});
+  const Task(this.nome, this.foto, this.dificuldade, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -57,33 +90,94 @@ class _TaskState extends State<Task> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        color: Colors.black,
         child: Stack(
           children: [
             Container(
-              color: Colors.blue,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.blue,
+              ),
               height: 140,
             ),
             Column(
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.white,
+                  ),
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.black26,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: Colors.black26),
                         width: 72,
                         height: 100,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.foto,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      Container(
-                          width: 200,
-                          child: Text(
-                            widget.nome,
-                            style: TextStyle(
-                                fontSize: 24, overflow: TextOverflow.ellipsis),
-                          )),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            child: Text(
+                              widget.nome,
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 1)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 2)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 3)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 4)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 5)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                       Container(
                         height: 64,
                         width: 64,
@@ -96,7 +190,8 @@ class _TaskState extends State<Task> {
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.blue,
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(4), // Remove as bordas arredondadas
+                              borderRadius: BorderRadius.circular(
+                                  4), // Remove as bordas arredondadas
                             ),
                           ),
                           child: Column(
@@ -107,7 +202,11 @@ class _TaskState extends State<Task> {
                                 Icons.arrow_drop_up,
                                 color: Colors.white,
                               ),
-                              Text("Up", style: TextStyle(fontSize: 12, color: Colors.white),),
+                              Text(
+                                "Up",
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.white),
+                              ),
                             ],
                           ),
                         ),
@@ -123,7 +222,9 @@ class _TaskState extends State<Task> {
                       child: Container(
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: nivel/10,
+                          value: (widget.dificuldade > 0)
+                              ? (nivel / widget.dificuldade / 10)
+                              : 1,
                         ),
                         width: 200,
                       ),
